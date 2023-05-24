1aba2efa5544e06ca88898bb803bf6ddb3891fe0
Jeffrey Lee Kedda <jeffece4560@gmail.com>
2023-05-24 12:21:52 -0400

    edited two-sum

--- two-sum.cpp
+++ two-sum.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int a, b = 0;
+          std::unordered_map<int, int> numMap;
 
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = i + 1; j < nums.size(); j++){
-                if(nums[i] + nums[j] == target){
-                    a = i;
-                    b = j;
-                }
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+
+            if (numMap.find(complement) != numMap.end()) {
+                return {numMap[complement], i};
             }
 
+            numMap[nums[i]] = i;
         }
-        return {a, b};
+
+        return {}; // Return an empty vector if no solution is found
     }
 };
