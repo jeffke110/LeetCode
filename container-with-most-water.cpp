cb9a2be7de6da523e3f35fe644809d1edf6d1433
Jeffrey Lee Kedda <jeffece4560@gmail.com>
2023-05-25 11:14:17 -0400

    added container with most water problem

---
+++ container-with-most-water.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.size() - 1;
+        
+        while (left < right) {
+            int h = min(height[left], height[right]);
+            int w = right - left;
+            int area = h * w;
+            maxArea = max(maxArea, area);
+            
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        
+        return maxArea;
+    }
+
+     //max_height = max(height[i], max_height);
+};
