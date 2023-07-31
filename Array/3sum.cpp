02e4fc383b45fe16f5ba266454cd4496d0748cba
jeffke110 <jeffke@vt.edu>
2023-07-31 15:22:32 -0400

    edited 3sum file

--- Array/3sum.cpp
+++ Array/3sum.cpp
@@ -14,34 +14,35 @@ public:
         for (int i = 0; i < n - 2; i++) {
             // Skip duplicates for the first number
             if(i > 0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            int left = i + 1;
-            int right = n - 1;
+            
+                
+                int left = i + 1;
+                int right = n - 1;
 
-            while(left < right){
-                int sum = nums[left] + nums[i] + nums[right];
+                while(left < right){
+                    int sum = nums[left] + nums[i] + nums[right];
 
-                if(sum < 0){
-                    left++;
-                }else if(sum > 0){
-                    right--;
-                }else{
-                    //sum == 0
-                    result.push_back({nums[i], 
-                    nums[left], nums[right] }
-                    );
-
-                    while(left < right && nums[left] == nums[left + 1]){
+                    if(sum < 0){
                         left++;
-                    }
-                    while(left < right && nums[right] == nums[right - 1]){
+                    }else if(sum > 0){
+                        right--;
+                    }else{
+                        //sum == 0
+                        result.push_back({nums[i], 
+                        nums[left], nums[right] }
+                        );
+                        //checking duplicates
+                        while(left < right && nums[left] == nums[left + 1]){
+                            left++;
+                        }
+                        while(left < right && nums[right] == nums[right - 1]){
+                            right--;
+                        }
+                        left++;
                         right--;
                     }
-                     left++;
-                    right--;
-                }
 
+                }
             }
 
         }
