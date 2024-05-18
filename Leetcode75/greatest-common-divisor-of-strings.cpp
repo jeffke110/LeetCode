0ce0c39513fa3807e4497a99c283fe946504bd20
jeffke110 <jlkedda@gmail.com>
2024-05-18 10:24:38 -0400

    greatest-common-divisor-of-strings

--- Leetcode75/greatest-common-divisor-of-strings.cpp
+++ Leetcode75/greatest-common-divisor-of-strings.cpp
@@ -1,17 +1,24 @@
+
+#include<bits/stdc++.h>
 #include <iostream>
-#include <string>
 #include <algorithm>
+using namespace std;
 
+/*
+For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
 
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+*/
 class Solution
 {
-public:
     std::string gcdOfStrings(std::string str1, std::string str2)
     {
-        if (str1 + str2 != str2 + str1){
+        if (str1 + str2 != str2 + str1)
+        {
             return "";
         }
-        int gcd = __gcd(str1.length(), str2.length());
-        return str1.substr(0, gcd);
+        int length = __gcd(str1.size(), str2.size());
+        return str1.substr(0, length);
     }
 };
