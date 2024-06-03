863bd3438e6d12a21876928e4e5e3b80f6b361c7
jeffke110 <jlkedda@gmail.com>
2024-06-03 12:27:04 -0400

    longest-subarray-of-1s-after-deleting-one-element

---
+++ Leetcode75/longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+
+class Solution {
+public:
+    int longestSubarray(std::vector<int>& nums) {
+        int left = 0;
+        int right = 0; 
+        int currentCount = 0;
+
+        
+        for(right = 0; right < nums.size(); right++){
+         
+        }
+
+        return right - left;
+
+
+    }
+};
