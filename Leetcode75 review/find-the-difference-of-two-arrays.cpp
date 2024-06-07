fc94d18f9317dbf4770f9ead68769516981b768e
jeffke110 <jlkedda@gmail.com>
2024-06-07 15:25:18 -0400

    find-the-difference-of-two-arrays

---
+++ Leetcode75 review/find-the-difference-of-two-arrays.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <set>
+#include <algorithm>
+
+class Solution
+{
+public:
+    std::vector<std::vector<int>> findDifference(std::vector<int> &nums1, std::vector<int> &nums2)
+    {
+        std::set<int> set1(nums1.begin(), nums1.end());
+        std::set<int> set2(nums2.begin(), nums2.end());
+        std::vector<int> diff1;
+        std::vector<int> diff2;
+
+        std::set_difference(set1.begin(), set1.end(), set2.begin(), set2.end(), std::back_inserter(diff1));
+        std::set_difference(set2.begin(), set2.end(), set1.begin(), set1.end(), std::back_inserter(diff2));
+
+        return {diff1, diff2};
+
+    }
+};
